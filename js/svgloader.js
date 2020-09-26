e86a73fdb2e8cd0a6b01a00efa09a9d1f68077ef
root <root@K3.lan>
2020-09-27 00:10:23 +0800

    update common.css

--- js/svgloader.js
+++ js/svgloader.js
@@ -5,7 +5,7 @@ const svgLoader = (a) => {
 
 	try {
 		ajax = new XMLHttpRequest();
-		ajax.open('GET', '../svg'+name+'.svg', true);
+		ajax.open('GET', 'https://cdn.jsdelivr.net/gh/70599/static-files/images/svg/'+name+'.svg', true);
 		ajax.send();
 		ajax.onload = () => {
 			if (ajax.status >= 200 && ajax.status < 400) {
