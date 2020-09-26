268cf05c48a7703e66213bc059011040f19cbaa8
root <root@K3.lan>
2020-09-27 00:24:45 +0800

    update common.css

--- js/svgloader.js
+++ js/svgloader.js
@@ -3,16 +3,20 @@
 const svgLoader = (a) => {
 	try {
 		var name = a.getAttribute('name'),
-		    ajax = new XMLHttpRequest();
+		    ajax = new XMLHttpRequest(),
+				icon;
 
 		ajax.open('GET', 'https://cdn.jsdelivr.net/gh/70599/static-files/images/svg/'+name+'.svg', true);
 		ajax.send();
 		ajax.onload = () => {
 			if (ajax.status >= 200 && ajax.status < 400) {
-				return ajax.responseText;
+				icon = ajax.responseText;
 			}
 		}
 	} catch (err) {
 		console.log(err);
 	}
+
+	console.log(icon);
+	return icon;
 }
