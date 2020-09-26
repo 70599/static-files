a58f6f5f474a6f3a02ca998f71fa59c46ea6fc81
root <root@K3.lan>
2020-09-27 00:38:15 +0800

    update common.css

--- js/svgloader.js
+++ js/svgloader.js
@@ -1,20 +1,17 @@
 'use strict';
 
 const svgLoader = (a) => {
-	try {
-		var name = a.getAttribute('name'),
-		    ajax = new XMLHttpRequest(),
-				icon;
+	var name = a.getAttribute('name'),
+	    ajax = new XMLHttpRequest(),
+			icon;
 
-		ajax.open('GET', 'https://cdn.jsdelivr.net/gh/70599/static-files/images/svg/'+name+'.svg', true);
-		ajax.send();
-		ajax.onload = () => {
-			console.log(ajax.status);
-			icon = ajax.responseText;
-			console.log(icon);
-			return icon;
-		}
-	} catch (err) {
-		console.log(err);
+	ajax.open('GET', 'https://cdn.jsdelivr.net/gh/70599/static-files/images/svg/'+name+'.svg', true);
+	ajax.send();
+	ajax.onload = (e) => {
+		console.log(ajax.status);
+		icon = ajax.responseText;
+		console.log(e);
+		console.log(icon);
+		return icon;
 	}
 }
