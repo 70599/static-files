4a6c8aae02032e3c11b12eb1eab922f10df9f609
root <root@K3.lan>
2020-11-15 23:26:15 +0800

    update common.css

--- js/common.js
+++ js/common.js
@@ -70,6 +70,29 @@ const qrcodeSvg = (el) => {
 	div.innerHTML = svg;
 }
 
+function parseQuery(queryString) {
+    var query = {};
+    var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+    for (var i = 0; i < pairs.length; i++) {
+        var pair = pairs[i].split('=');
+        query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+    }
+    return query;
+}
+
+// convert queryString to object
+const parseQuery = (queryString) => {
+	var query = {},
+	    pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+
+	for (var i = 0; i < pairs.length; i++) {
+		var pair = pairs[i].split('=');
+		query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+	}
+
+	return query;
+}
+
 /*
 |-------------------------------------------------------------------------------
 |	Action(s)
