b76f5e2c6ea042551a79037180e1ba5fc44e969b
root <root@K3.lan>
2020-11-17 13:53:28 +0800

    update common.css

--- js/common.js
+++ js/common.js
@@ -88,6 +88,7 @@ const toggleCheckBox = (el, valA, valB) => {
 	document.querySelectorAll(el).forEach(check_box => {
 		check_box.addEventListener('change', (event) => {
 			check_box.value = check_box.checked ? valA : valB;
+			console.log(check_box.value);
 		});
 	});
 }
